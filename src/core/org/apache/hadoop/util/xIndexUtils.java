acb0e50b445af2e573d05525f4e48bc234671804
Manuel Gomes Ferreira <manuelssgomesferreira@gmail.com>
2014-05-02 16:14:35 +0100

    index

--- src/core/org/apache/hadoop/util/xIndexUtils.java
+++ src/core/org/apache/hadoop/util/xIndexUtils.java
@@ -399,6 +399,6 @@ public class xIndexUtils {
 	
 	public static String toHex(byte[] bytes) {
 	    BigInteger bi = new BigInteger(1, bytes);
-	    return String.format("%0" + (bytes.length << 1) + "X", bi);
+	    return String.format("%0" + (bytes.length << 1) + "X", bi).substring(0, 2);
 	}
 }
